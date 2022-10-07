06714e09dde47674dddb59c6ac6516125dcc4587
damin <damin0320@gmail.com>
2022-10-07 22:42:24 +0900

    ADD : input 빈칸 관리

--- src/components/AddForm.jsx
+++ src/components/AddForm.jsx
@@ -24,20 +24,23 @@ const [todo, setTodo] = useState(
       // 하나만 넣으면 undefined가 뜬다.
       return alert("빈칸을 입력해주세요")
     }
-
     dispatch(addTodo({
       id : uuidv4(),
       title : todo.title,
       body : todo.body, 
       isDone : false,
     }))
+    setTodo(() => {
+      return {...todo, title: "", body : ""}
+    })
+    // 깊은 복사로 빈값으로 바꾸기
   }
   return (
     <StFormContainer>
       <form onSubmit={onSubmitHandler}>
         <label>Todos의 제목과 내용을 입력하세요</label>
-        <StInput type="text" name="title" value={todos.title} onChange={setChangeHandler} />
-        <StInput type="text" name="body" value={todos.body} onChange={setChangeHandler} />
+        <StInput type="text" name="title" value={todo.title} onChange={setChangeHandler} />
+        <StInput type="text" name="body" value={todo.body} onChange={setChangeHandler} />
         <StButton>추가하기</StButton>
       </form>
     </StFormContainer>
